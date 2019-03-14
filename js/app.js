68eb9cb6a502c68559d169afabcfedc7d1804c40
Carlos <carcl69@hotmail.com>
2019-03-13 18:22:52 -0600

    Ingresa al display los numeros y limita a 8 los número

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var imagenes;
 var imagenes = document.getElementsByTagName('img');// Obtenemos las imágenes
 var display = document.getElementById('display');// Obtenemos el display lo guardamos en una variable
 var dis = document.getElementById('display').innerText;//Obtenemos el contenido thead-default display
+var verdadero = false;
 
 //El ciclo for les activa la función a las imagenes de on mousedown and onmouseup
 for(var i=0; imagenes.length; i++){
@@ -17,26 +18,42 @@ function mouseDown(event){
   //document.getElementById(seleccion).style = "width: 21%;"
   document.getElementById(seleccion).style = "filter: blur(5px); -webkit-filter:blur(5px)"
   dis = document.getElementById('display').innerText;
-  arreglo = Array.from(dis);
+  //Convierte los numeros en el display a un array
+  arregloDisplay = Array.from(dis);
+
+  //Aquí deja agregar un punto nada más
+  if(verdadero){
+    seleccion = "." + seleccion;
+    verdadero = false;
+  }
+  if(dis.indexOf(".") != -1 && seleccion == "punto"){
+    seleccion = "nada";
+  }
   if((seleccion>=0 && seleccion<10) || seleccion=="punto"){
-
-
     if(seleccion == "punto"){
+      //Aqui lo hace verdadero y se queda guardado el punto para el siguiente numero ingresado ambos se puedan concatenar
       verdadero = true;
     }else{
         //Convierte en flotante el exponencial extraido del display
-          console.log(dis);
+        if(arregloDisplay.length>7){
           dis = parseFloat(dis);
+        }
+        //Concatena la cadena que ya existia con el numero presionado
         numero = dis+seleccion;
-
 
         if(dis=="0"){
           //Elimina el primer cero convirtiendo a número el texto extraido del display
-          numero = Number(numero);
+          numero = parseFloat(numero);
         }
     }
-
+    //Muestra el múmero en el display si es mas de 7 caracteres lo convierte a exponencial
+    if(arregloDisplay.length>7){
+      numero = parseFloat(numero);
+      numero = numero.toExponential(4);
+    }
     display.innerHTML =   numero;
+  }else if(seleccion == "on"){ //Pone a cero el display de la calculadora
+    display.innerHTML = "0";
   }
 }
 
@@ -50,17 +67,21 @@ function mouseUp(event){
 //Funcion le hace un blur a la imagen cuando se teclea
 function oprimir_tecla(event){
     var tecla = event.which || event.keyCode;
+    dis = document.getElementById('display').innerText;
     seleccion = String.fromCharCode(tecla);
       console.log(seleccion);
       seleccion = entradaTeclado(seleccion);
     document.getElementById(seleccion).style = "filter: blur(5px); -webkit-filter:blur(5px)"
+    timeout(seleccion);
 }
 //Funcion le quita el blur a la imagen cuando se suelta
-function soltar_tecla(event){
-    seleccion = entradaTeclado(seleccion);
-    //Al dejar de presionar le quita el blur a la tecla la tecla / dividir no funciona en firefox me sale buscar en chrome funciona bien
-    document.getElementById(seleccion).style = "filter: blur(0px); -webkit-filter:blur(0px)"
+
+function timeout(seleccion){
+  setTimeout(function(){
+      document.getElementById(seleccion).style="filter: blur(0px); -webkit-filter:blur(0px)";
+  },500,"JavaScript");
 }
+
 
 //Convierte el caractér del teclado en el id de la imagen
 function entradaTeclado(seleccion){
