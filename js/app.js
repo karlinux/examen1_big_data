c02f5f3d62afd1f3be9a8a84f17d47c179e63321
Carlos <carcl69@hotmail.com>
2019-03-14 10:50:07 -0600

    Ya terminado

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 document.onkeypress = oprimir_tecla;
+var notas = document.getElementsByClassName('nota-input');
 var imagenes = document.getElementsByTagName('img');// Obtenemos las imágenes
 var display = document.getElementById('display');// Obtenemos el display lo guardamos en una variable
 var ContenidoDisplay = document.getElementById('display').innerText;//Obtenemos el contenido thead-default display
 var guardarPunto = false;
 var signo = false;
 var numero = 0;
+var operarbooleano = false;
 
+notas[0].value = 'Para usar la calculadora con el teclado la tecla ON se usa con la letra o y la tecla +- on la s de signo'+
+'las demás normal. \nEn firefox en mac cuando uso la tecla / para la división me abre el buscador entonces pierde el foco el'+
+'navegador y la letra se quedaba presionada mejor les puse un timer para que se deseleccionen solas. \nLe puse un blur en vez de '+
+'que se hiciera más pequeña al presionar';
 //El ciclo for les activa la función a las imagenes de on mousedown and onmouseup
 for(var i=0; imagenes.length; i++){
 imagenes[i].onmousedown = mouseDown;
@@ -29,24 +35,26 @@ function mouseDown(event){
   if(ContenidoDisplay.indexOf(".") != -1 && seleccion == "punto"){
     seleccion = "nada";
   }
-console.log(seleccion);
+
 //Esta parte coloca los numeros en el display solo numeros y el punto
   if((seleccion>=0 && seleccion<10) || seleccion=="punto"){
     display.innerHTML =   displays(seleccion);
   }else if(seleccion == "on"){
+    //Limpia lo guardado en local estorage y coloca en cero el display
+    localStorage.removeItem("calcular");
+    numero="0";
     display.innerHTML = "0";
   }else if(seleccion=="sign" && ContenidoDisplay!="0"){
     display.innerHTML =   signos(seleccion);
   }else if(seleccion == "por"){
-    display.innerHTML =   calcular("*");
+    calcular("*");
   }else if(seleccion == "dividido"){
-    display.innerHTML =   calcular("/");
+    calcular("/");
   }else if(seleccion == "menos"){
-    display.innerHTML =   calcular("-");
+    calcular("-");
   }else if(seleccion == "mas"){
-    display.innerHTML =   calcular("+");
+    calcular("+");
   }else if(seleccion == "igual"){
-
     display.innerHTML = igual();
   }
 }
@@ -61,43 +69,65 @@ function calcular(operacion){
   return 0;
 
 }
-
+//Función devuelve el resultado
 function igual(){
+  var resultado = "0";
   var calcular = JSON.parse(localStorage.getItem('calcular'));
   switch(calcular.operacion){
     case "*":
-    numero = Number(calcular.numero) * Number(ContenidoDisplay);
+    resultado = parseFloat(calcular.numero) * parseFloat(ContenidoDisplay);
     break;
     case "/":
-    numero = Number(calcular.numero) / Number(ContenidoDisplay);
+    resultado = parseFloat(calcular.numero) / parseFloat(ContenidoDisplay);
     break;
     case "-":
-    numero = Number(calcular.numero) - Number(ContenidoDisplay);
+    resultado = parseFloat(calcular.numero) - parseFloat(ContenidoDisplay);
     break;
     case "+":
-    numero = Number(calcular.numero) + Number(ContenidoDisplay);
+    resultado = parseFloat(calcular.numero) + parseFloat(ContenidoDisplay);
     break;
   }
+  //Convierte en un arreglo el resultado
+  arregloResultado = Array.from(resultado.toString());
 
+  //Búsca si es negativo el resultado para que no le ponga otro signo negativo
+  if(arregloResultado.includes("-")){
+    signo = true;
+  }else{
+    signo = false;
+  }
 
-  if(arregloDisplay.length>8){
-    numero = parseFloat(numero);
-    numero = numero.toExponential(4);
+  //Si el resultado en mas de 8 digitos lo convierte enexponencial
+  if(arregloResultado.length>8){
+    resultado = parseFloat(resultado);
+    resultado = resultado.toExponential(4);
   }
-  return numero;
-  }
+  //Esta variable no deja borrar hasta que se ingresa el primer número
+  operarbooleano=true;
+  return resultado;
+
+  setTimeout(function(){
+    resultado = "0";
+  },1000)
+}
+
 //Funcion para los numeros y punto mostrados en el display ----------
 function displays(seleccion){
   //Guarda un punto para que se le asigne antes al siguiente número
-  if(seleccion == "punto"){
+  if(seleccion == "punto" || seleccion == "." ){
     guardarPunto = true;
   }else{
       //Convierte en flotante el exponencial extraido del display
       if(arregloDisplay.length>7){
         ContenidoDisplay = parseFloat(ContenidoDisplay);
       }
-      //Concatena la cadena que ya existia con el numero presionado
 
+      //Borra despues de una operacion hasta que se ingresa el primer número
+      if(operarbooleano){
+        ContenidoDisplay="0";
+        operarbooleano=false;
+      }
+      //Concatena la cadena que ya existia con el numero presionado
       numero = ContenidoDisplay+seleccion;
 
 
@@ -121,7 +151,7 @@ function signos(){
     // Elimina el signo -
     numero = remplazar(ContenidoDisplay);
     signo = false
-    console.log(ContenidoDisplay);
+
   }else{
     //Convierte el número en negativo o agrega el signo
     numero = "-" + ContenidoDisplay;
@@ -129,6 +159,7 @@ function signos(){
   }
   return numero;
 }
+
 //Función para eliminar el signo -
 function remplazar(str){
   var str = str.replace("-", "")
@@ -143,7 +174,8 @@ function calcular(operacion){
   }
   //Guarda en un local storage el primer numero que se encuentra en el display y el operador
   localStorage.setItem('calcular', JSON.stringify(calcular));
-  
+  //Esta variable no deja borrar hasta que se ingresa el primer número
+  operarbooleano=true;
   return "0";
 }
 
@@ -160,6 +192,30 @@ function oprimir_tecla(event){
     ContenidoDisplay = document.getElementById('display').innerText;
     seleccion = String.fromCharCode(tecla);
 
+    arregloDisplay = Array.from(ContenidoDisplay);
+    //alert(seleccion + "  " + dis.indexOf("."));
+    if(guardarPunto){
+      seleccion = "." + seleccion;
+      guardarPunto = false;
+    }
+    // Esta parte es para evitar que se pongan mas de un punto en la calculadora
+    if(ContenidoDisplay.indexOf(".") != -1 && seleccion == "."){
+      seleccion = "nada";
+    }
+    //Esta parte coloca los numeros en el display solo numeros y el punto
+    if((seleccion>=0 && seleccion<10) || seleccion=="."){
+      display.innerHTML =   displays(seleccion);
+    }else if(seleccion == "o"){
+      //Limpia lo guardado en local estorage y coloca en cero el display
+      localStorage.removeItem("calcular");
+      display.innerHTML = "0";
+    }else if(seleccion=="s" && ContenidoDisplay!="0"){
+      display.innerHTML =   signos(seleccion); //Para agregar el signo 
+    }else if(seleccion == "*" || seleccion == "/" || seleccion == "-" || seleccion == "+"){
+      calcular(seleccion);
+    }else if(seleccion == "="){
+      display.innerHTML = igual();
+    }
       seleccion = entradaTeclado(seleccion);
     document.getElementById(seleccion).style = "filter: blur(5px); -webkit-filter:blur(5px)"
     timeout(seleccion);
